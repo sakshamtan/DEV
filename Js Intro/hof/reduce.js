f8e2b4128afea71d33b518411e1386149cac3c2d
Saksham <sakshamtaneja68@gmail.com>
2022-01-28 00:09:31 +0530

    update

---
+++ Js Intro/hof/reduce.js
@@ -0,0 +1,170 @@
+// reduce function -> jo callback function reduce use krta hai uske do variables hote hai 
+// 1st -> initial value / storage and 2nd -> iterator that iterates over the array
+
+let arr = [10, 20, 30, 40, 50];
+
+// Initially storage = 10 (arr[0]) and ele will iterate from arr[1] to arr[4] 
+function sum(storage, ele)
+{
+    return storage + ele;
+}
+
+function product(storage, ele)
+{
+    console.log("Storage:", storage, "element", ele);
+    return storage * ele;
+}
+
+// Case 1 -> Without initial value -> storage = arr[0] and ele will iterate from arr[1] to arr[4]
+// let finalProduct = arr.reduce(product);
+// console.log(finalProduct);
+
+// let finalSum = arr.reduce(sum);
+// console.log(finalSum);
+
+// Case 2 -> With initial value -> storage = initial value provided and ele will iterate from arr[0] to arr[4]
+let finalProduct = arr.reduce(product,11);
+console.log(finalProduct);
+
+let finalSum = arr.reduce(sum,11);
+console.log(finalSum);
+
+// Q -> Freecodecamp -> Use reduce, map and filter to find average of the imdbRatings of the movies directed by Christopher Nolan
+const watchList = [
+    {
+        "Title": "Inception",
+        "Year": "2010",
+        "Rated": "PG-13",
+        "Released": "16 Jul 2010",
+        "Runtime": "148 min",
+        "Genre": "Action, Adventure, Crime",
+        "Director": "Christopher Nolan",
+        "Writer": "Christopher Nolan",
+        "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page, Tom Hardy",
+        "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+        "Language": "English, Japanese, French",
+        "Country": "USA, UK",
+        "Awards": "Won 4 Oscars. Another 143 wins & 198 nominations.",
+        "Poster": "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+        "Metascore": "74",
+        "imdbRating": "8.8",
+        "imdbVotes": "1,446,708",
+        "imdbID": "tt1375666",
+        "Type": "movie",
+        "Response": "True"
+    },
+    {
+        "Title": "Interstellar",
+        "Year": "2014",
+        "Rated": "PG-13",
+        "Released": "07 Nov 2014",
+        "Runtime": "169 min",
+        "Genre": "Adventure, Drama, Sci-Fi",
+        "Director": "Christopher Nolan",
+        "Writer": "Jonathan Nolan, Christopher Nolan",
+        "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+        "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+        "Language": "English",
+        "Country": "USA, UK",
+        "Awards": "Won 1 Oscar. Another 39 wins & 132 nominations.",
+        "Poster": "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+        "Metascore": "74",
+        "imdbRating": "8.6",
+        "imdbVotes": "910,366",
+        "imdbID": "tt0816692",
+        "Type": "movie",
+        "Response": "True"
+    },
+    {
+        "Title": "The Dark Knight",
+        "Year": "2008",
+        "Rated": "PG-13",
+        "Released": "18 Jul 2008",
+        "Runtime": "152 min",
+        "Genre": "Action, Adventure, Crime",
+        "Director": "Christopher Nolan",
+        "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+        "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+        "Plot": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+        "Language": "English, Mandarin",
+        "Country": "USA, UK",
+        "Awards": "Won 2 Oscars. Another 146 wins & 142 nominations.",
+        "Poster": "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+        "Metascore": "82",
+        "imdbRating": "9.0",
+        "imdbVotes": "1,652,832",
+        "imdbID": "tt0468569",
+        "Type": "movie",
+        "Response": "True"
+    },
+    {
+        "Title": "Batman Begins",
+        "Year": "2005",
+        "Rated": "PG-13",
+        "Released": "15 Jun 2005",
+        "Runtime": "140 min",
+        "Genre": "Action, Adventure",
+        "Director": "Christopher Nolan",
+        "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+        "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+        "Plot": "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+        "Language": "English, Urdu, Mandarin",
+        "Country": "USA, UK",
+        "Awards": "Nominated for 1 Oscar. Another 15 wins & 66 nominations.",
+        "Poster": "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg",
+        "Metascore": "70",
+        "imdbRating": "8.3",
+        "imdbVotes": "972,584",
+        "imdbID": "tt0372784",
+        "Type": "movie",
+        "Response": "True"
+    },
+    {
+        "Title": "Avatar",
+        "Year": "2009",
+        "Rated": "PG-13",
+        "Released": "18 Dec 2009",
+        "Runtime": "162 min",
+        "Genre": "Action, Adventure, Fantasy",
+        "Director": "James Cameron",
+        "Writer": "James Cameron",
+        "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+        "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+        "Language": "English, Spanish",
+        "Country": "USA, UK",
+        "Awards": "Won 3 Oscars. Another 80 wins & 121 nominations.",
+        "Poster": "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+        "Metascore": "83",
+        "imdbRating": "7.9",
+        "imdbVotes": "876,575",
+        "imdbID": "tt0499549",
+        "Type": "movie",
+        "Response": "True"
+    }
+];
+
+// 1) Filter the movies directed by Christopher Nolan
+// 2) From the filtered movies take out only the imdbRatings in an array
+// 3) Find average of this array
+
+function getNolanMovies(movie)
+{
+    return movie.Director == "Christopher Nolan";
+}
+
+function getImdbRatings(movie)
+{
+    return movie.imdbRating;
+}
+
+function sum(storage, ele)
+{
+    return Number(storage) + Number(ele); // imdbRatings was in a string so converting it into int
+}
+
+let filteredMovies = watchList.filter(getNolanMovies);
+
+let totalSum = filteredMovies.map(getImdbRatings).reduce(sum);
+let avg = totalSum / filteredMovies.length;
+
+console.log(avg);
